1de7377564a9b0e409a07406878b4e3ab96ae777
laiapozo <pozotalarnlaia@gmail.com>
2025-03-06 11:21:46 +0100

    Add JS and styles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const taskList = document.querySelector(".js-task-list");
+
+const tasks = [
+  { name: "Recoger setas en el campo", completed: true, id: 1 },
+  { name: "Comprar pilas", completed: true, id: 2 },
+  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+  { name: "Aprender cómo se realizan las peticiones al servidor en JavaScript", completed: false, id: 4 },
+];
+
+for (const task of tasks) {
+    taskList.innerHTML +=`<li>${task.name}</li>`;
+}
