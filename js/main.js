d4543191880ce3a865e72249c72e02b04c41dff9
Paloma Fernandez Jaen <palomafj91@gmail.com>
2025-03-10 11:23:41 +0100

    cambiando cosas despeseracion

--- js/main.js
+++ js/main.js
@@ -2,44 +2,51 @@
 
 const taskList = document.querySelector(".js-task-list");
 
-const tasks = [
-  { name: "Recoger setas en el campo", completed: true, id: 1 },
+let tasks = [
+  /*{ name: "Recoger setas en el campo", completed: true, id: 1 },
   { name: "Comprar pilas", completed: true, id: 2 },
   { name: "Poner una lavadora de blancos", completed: true, id: 3 },
   {
     name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
     completed: false,
     id: 4,
-  },
+  },*/
 ];
+  const GITHUB_USER = "PalomaFJ";
+  const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 
-for (const task of tasks) {
-  if (task.completed === true) {
-    taskList.innerHTML += `<li class="completed"><input id="${task.id}" type="checkbox" checked="">${task.name}</li>`;
-  } else {
-    taskList.innerHTML += `<li><input id="${task.id}" type="checkbox" checked="">${task.name}</li>`;
+  fetch(SERVER_URL)
+  .then(response => response.json())
+  .then(data=> {
+    tasks = data.results;
+
+     renderTasks(tasks)
+    
+  })
+
+
+  function renderTasks(list) {
+
+    for (const task of list) {
+      if (task.completed === true) {
+        taskList.innerHTML += `<li class="completed"><input id="${task.id}" type="checkbox" checked="">${task.name}</li>`;
+      } else {
+        taskList.innerHTML += `<li><input id="${task.id}" type="checkbox" >${task.name}</li>`;
+      }
+    }
   }
-}
-
+  
 
 const handleClick = (event) => {
   const taskID = parseInt(event.target.id);
-  if (!taskID) return; 
-  if (tasks[taskID - 1].completed === true) {
-    tasks[taskID - 1].completed === false;
+  if (!taskID) return;
+  const chosenTask = tasks.find(task=> task.id === taskID);
+  if (chosenTask.completed === true){
+    chosenTask.completed === false
   } else {
-    tasks[taskID - 1].completed === true;
-  } 
-
-  taskList.innerHTML =" ";
-
-  for (const task of tasks) {
-    if (task.completed === true) {
-      taskList.innerHTML += `<li class="completed"><input id="${task.id}" type="checkbox" checked="">${task.name}</li>`;
-    } else {
-      taskList.innerHTML += `<li><input id="${task.id}" type="checkbox" checked="">${task.name}</li>`;
-    }
+    chosenTask.completed === true 
   }
+  renderTasks(tasks)
 };
 
 taskList.addEventListener("click", handleClick);
